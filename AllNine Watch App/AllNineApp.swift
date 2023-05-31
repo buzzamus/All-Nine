b18e3db66cddd840e2107add44d977ab4cfdb928
buzzamus <brent.busby@yahoo.com>
2023-05-30 20:05:47 -0500

    initial commit

---
+++ AllNine Watch App/AllNineApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllNineApp.swift
+//  AllNine Watch App
+//
+//  Created by Brent Busby on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct AllNine_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
